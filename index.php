ae6c17eb11f97f3d2794153c3b96d9afde72ebe8
skr1pmen <KiberKot.v.roymenge@gmail.com>
2023-07-29 18:46:30 +0400

    доделка

--- index.php
+++ index.php
@@ -8,8 +8,18 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Главная</title>
+        <link rel="stylesheet" href="./assets/styles/style.css">
     </head>
     <body>
         <?php require './moduls/header.php'?>
+        <div class="container">
+            <div class="item">
+                <img src="" alt="">
+                <h4 class="title_item">Название товара</h4>
+                <div class="rating">4.5⭐ </div>
+                <span class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa eaque eveniet incidunt perspiciatis quia soluta.</span>
+                <a href="#">Подробнее</a>
+            </div>
+        </div>
     </body>
 </html>
