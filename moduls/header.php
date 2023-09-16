293a6177ac0c85fa2d6aed5447a8804a03cabb8a
skr1pmen <KiberKot.v.roymenge@gmail.com>
2023-09-16 18:39:31 +0400

    16

--- moduls/header.php
+++ moduls/header.php
@@ -2,22 +2,21 @@
 $id = $_SESSION['user_id'];
 //var_dump($_SERVER['REQUEST_URI']);
 if ($_SERVER['REQUEST_URI'] != '/') $home = '../'; else $home = './';
-if (!empty($id)){
-    if ($_SERVER['REQUEST_URI'] != '/') $profile = '../profile.php?id='.$id; else $profile = './pages/profile.php?id='.$id;
-} else{
+if (!empty($id)) {
+    if ($_SERVER['REQUEST_URI'] != '/') $profile = '../profile.php?id=' . $id; else $profile = './pages/profile.php?id=' . $id;
+} else {
     if ($_SERVER['REQUEST_URI'] != '/') $profile = './login.php'; else $profile = './pages/login.php';
 }
-if ($_SESSION['is_admin'] == true){
+if ($_SESSION['is_admin'] == true) {
     if ($_SERVER['REQUEST_URI'] != '/') $add = './add_product.php'; else $add = './pages/add_product.php';
-} else{
+} else {
     $add = $home;
 }
 $productsForSearch = select('SELECT title FROM products');
 
 ?>
-
 <header>
-    <a href="<?=$home?>">CloudShop</a>
+    <a href="<?= $home ?>">Byketberry</a>
     <form action="../php/search.php" method="get">
         <input name="name" type="text" list="search" placeholder="Поиск">
         <input type="submit" value="Поиск">
@@ -25,8 +24,12 @@ $productsForSearch = select('SELECT title FROM products');
     <datalist id="search">
         <?php foreach ($productsForSearch as $title): ?>
             <option value="<?= $title['title'] ?>"></option>
-        <?php endforeach;?>
+        <?php endforeach; ?>
     </datalist>
-    <a href="<?php echo $add?>">Добавление товара</a>
-    <a href="<?=$profile?>">G</a>
+    <a href="<?php echo $add ?>">Добавление товара</a>
+    <a href="">О нас</a>
+    <a href="">Контакты</a>
+    <a href="<?= $profile ?>">💼</a>
 </header>
+
+
